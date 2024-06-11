d73614e2bfeaaf09fdf24dd068de4da942e2398f
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-06-11 15:54:00 +0530

    Blog List Frontend, step 4

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import getUser from './services/login';
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState({
+    message: null,
+    isError: false,
+  });
 
   useEffect(() => {
     blogService.getAll().then((blogs) => {
@@ -30,36 +34,60 @@ const App = () => {
 
   const addNewBlog = async (newBlog) => {
     const token = user.token;
-
-    const addedBlog = await blogService.addNew(newBlog, token);
-    setBlogs(blogs.concat(addedBlog));
+    try {
+      const addedBlog = await blogService.addNew(newBlog, token);
+      setBlogs(blogs.concat(addedBlog));
+      userNotification('Blog added!', false);
+    } catch (exception) {
+      userNotification('Please fill in the correct fields', true);
+    }
   };
 
   const logOutUser = () => {
     window.localStorage.removeItem('loggedInUser');
     setUser(null);
+    userNotification('Logged out!', false);
+  };
+
+  const userNotification = (message, isError) => {
+    setNotification({ message: message, isError: isError });
+    setTimeout(() => {
+      setNotification({ message: null, isError: false });
+    }, 5000);
   };
 
   if (user) {
     return (
       <>
         <LoggedInUserInfo username={user.username} logOutUser={logOutUser} />
+        <Notification notification={notification} />
         <NewBlogForm addNewBlog={addNewBlog} />
         <BlogList blogs={blogs} />
       </>
     );
   }
 
-  return <LoginForm loginUser={loginUser} />;
+  return (
+    <>
+      <Notification notification={notification} />
+      <LoginForm loginUser={loginUser} userNotification={userNotification} />
+    </>
+  );
 };
 
-const LoginForm = ({ loginUser }) => {
+const LoginForm = ({ loginUser, userNotification }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const loginButtonAction = (event) => {
+  const loginButtonAction = async (event) => {
     event.preventDefault();
-    loginUser({ username, password });
+    try {
+      await loginUser({ username, password });
+      userNotification('Logged in!', false);
+    } catch (exception) {
+      console.log(exception.response.data.error);
+      userNotification(exception.response.data.error, true);
+    }
   };
 
   return (
@@ -157,6 +185,30 @@ const NewBlogForm = ({ addNewBlog }) => {
       <button onClick={() => addNewBlog(newBlog)}>Add blog</button>
     </>
   );
+};
+
+const Notification = ({ notification }) => {
+  if (notification.message) {
+    const styles = {
+      height: '50px',
+      display: 'flex',
+      'align-items': 'center',
+      'justify-content': 'center',
+    };
+
+    if (notification.isError) {
+      styles['border'] = '8px solid red';
+      styles['background-color'] = '#f48b95';
+    } else {
+      styles['border'] = '8px solid green';
+      styles['background-color'] = '#8cf78e';
+    }
+    return (
+      <div style={styles}>
+        <h3>{notification.message}</h3>
+      </div>
+    );
+  }
 };
 
 export default App;
