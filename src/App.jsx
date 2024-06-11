3298cd57199b2b2deafb364b6ccff7cf8d8e7055
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-06-11 11:28:59 +0530

    Blog List Frontend, step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,73 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect, useSyncExternalStore } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import getUser from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const loginUser = async (userObject) => {
+    const loggedInUser = await getUser(userObject);
+    setUser(loggedInUser);
+  };
+
+  if (user) {
+    return BlogList(user.username, blogs);
+  }
+
+  return <LoginForm loginUser={loginUser} />;
+};
+
+const LoginForm = ({ loginUser }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginButtonAction = (event) => {
+    event.preventDefault();
+    loginUser({ username, password });
+  };
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+    <>
+      <h2>Log into application</h2>
+      <form onSubmit={loginButtonAction}>
+        <label htmlFor="username">Username: </label>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
 
-export default App
+        <label htmlFor="password">Password: </label>
+        <input
+          id="password"
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+const BlogList = (username, blogs) => {
+  return (
+    <div>
+      <h1>blogs</h1>
+      <p>{username} is logged in</p>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/services/login.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+const baseUrl = '/api/login';
+
+const getUser = async (userObject) => {
+  const response = await axios.post(baseUrl, userObject);
+  return response.data;
+};
+
+export default getUser;
