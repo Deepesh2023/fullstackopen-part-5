e94a935d69a305f552a441bea00d40425478ca5f
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-06-12 11:37:39 +0530

    Blog List Frontend, step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    border: '2px solid black',
+    padding: '4px',
+    marginBottom: '4px',
+  };
+
+  if (showDetails) {
+    return (
+      <div style={blogStyle}>
+        <h4>{blog.title}</h4>
+        <p>{blog.url}</p>
+        <p>{blog.author}</p>
+        <p>
+          Likes: {blog.likes}
+          <button>Like</button>
+        </p>
+        <button onClick={() => setShowDetails(!showDetails)}>Hide</button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      <h4>{blog.title}</h4>
+      <button onClick={() => setShowDetails(!showDetails)}>View</button>
+    </div>
+  );
+};
+
+export default Blog;
